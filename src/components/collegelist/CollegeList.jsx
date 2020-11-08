281acc243e08949ba019ddf43d955a0dedeb274e
GandharvJaggi <gandharv.jaggi@gmail.com>
2020-11-08 15:30:46 +0530

    fix

--- src/components/collegelist/CollegeList.jsx
+++ src/components/collegelist/CollegeList.jsx
@@ -7,27 +7,31 @@ import useIntersect from '../../hooks/useIntersect';
 export default function CollegeList() {
   const listdata = data.colleges;
   const [index, setindex] = useState(1);
+  const [loading, setloading] = useState(false);
   const [list, setList] = useState(listdata.slice(0, 10));
 
-  const [ref, entry] = useIntersect({threshold: [1.0]});
+  const [ref, entry] = useIntersect({threshold: 1.0, rootMargin: '0px'});
 
   const getList = useCallback(() => {
     // setList()
     setindex(index + 1);
     setList(listdata.slice(0, index * 10));
+    setloading(false)
   }, [index, listdata]);
 
   useEffect(() => {
     console.log(entry.intersectionRatio);
-    if (entry.intersectionRatio === 1.0) {
+    if (entry.intersectionRatio === 1) {
+      setloading(true)
       getList();
     }
   }, [entry, getList]);
   return (
-    <div ref={ref} id='collegelist'>
-      {list.map((d,i) => (
+    <div id='collegelist'>
+      {list.map((d, i) => (
         <CollegeCard key={i} {...d} />
       ))}
+      <div className={`loading ${loading ? 'hide' : ''}`} ref={ref}>loading</div>
     </div>
   );
 }
